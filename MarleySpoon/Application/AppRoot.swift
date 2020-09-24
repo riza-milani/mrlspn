bd2019b0c91a4cec4546c2659b1ed1baa34cda1d
riza <rizamili3@gmail.com>
2020-09-24 16:32:49 +0330

    Basic initialization and grouping

---
+++ MarleySpoon/Application/AppRoot.swift
@@ -0,0 +1,27 @@
+//
+//  AppRoot.swift
+//  MarleySpoon
+//
+//  Created by riza milani on 7/3/1399 AP.
+//  Copyright © 1399 reza milani. All rights reserved.
+//
+
+import UIKit
+
+protocol AppRootProtocol {
+    static func initRootWindow() -> UIWindow
+}
+
+class AppRoot: AppRootProtocol {
+
+    static func initRootWindow() -> UIWindow {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let recipeListRouter = RecipeListRouter()
+        let destinationViewController = recipeListRouter.assemble()
+        let navigationContoller = UINavigationController(rootViewController: destinationViewController)
+        navigationContoller.modalPresentationStyle = .fullScreen
+        window.rootViewController = navigationContoller
+        window.makeKeyAndVisible()
+        return window
+    }
+}
---
+++ MarleySpoon/Modules/RecipeList/Router/RecipeListRouter.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListRouter.swift
+//  MarleySpoon
+//
+//  Created by riza milani on 7/3/1399 AP.
+//  Copyright © 1399 reza milani. All rights reserved.
+//
+
+import UIKit
+
+class RecipeListRouter {
+
+    func assemble() -> UIViewController {
+        let viewController = ViewController()
+        return viewController
+    }
+}
